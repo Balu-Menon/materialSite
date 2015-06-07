1020419c9819010c099c86ac0e7f3417bb5a68f8
Balu-Menon <balbbrl@gmail.com>
2015-06-07 13:52:56 +0530

    third

--- js/main.js
+++ js/main.js
@@ -1,7 +1,22 @@
-$(window).load(function(){
-    var $container=$('.isotope').isotope(
-        {
-            layoutMode:'fitRows'
+$(document).ready( function() {  
+    $('.isotope').isotope({
+        layoutMode: 'packery',
+        itemSelector: '.col',
+        packery: {
+            gutter: 0
         }
-    );
+    });
+});
+
+$(document).ready(function() {
+    $('#right-menu').sidr({
+      name: 'sidr-right',
+      side: 'right'
+    });
+});
+
+$(document).ready(function(){
+    $('.btn-floating').click(function(){
+        $('.search').slideToggle();
+    });
 });
